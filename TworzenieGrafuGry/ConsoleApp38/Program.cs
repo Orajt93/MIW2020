5b5c967964a8053ee1614983469d63bd52c9b6ee
Orajt93 <57015022+Orajt93@users.noreply.github.com>
2020-05-03 10:16:13 +0200

    To samo

---
+++ TworzenieGrafuGry/ConsoleApp38/Program.cs
@@ -0,0 +1,34 @@
+﻿using DotNetGraph;
+using DotNetGraph.Edge;
+using DotNetGraph.Extensions;
+using DotNetGraph.Node;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace ConsoleApp38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating first node
+            Node firstNode = new Node() { Player = "prot" };
+            firstNode.drawNode = new DotNode($"Node{Node.nodeCount}") 
+             { Label = $"{firstNode.Player}{firstNode.Value}" };
+            StaticGraph.graph.Elements.Add(firstNode.drawNode);
+            Node.nodeCount++;
+
+            //Lets go!
+            Node.CreateTree(firstNode);
+
+            Console.ReadKey();
+
+            //Create and compile graph
+            var dot = StaticGraph.graph.Compile(true);
+            File.WriteAllText("input.dot", dot);
+            Console.WriteLine("finito");   
+        }
+    }
+}
---
+++ TworzenieGrafuGry/ConsoleApp38/StaticGraph.cs
@@ -0,0 +1,17 @@
+﻿using DotNetGraph;
+using DotNetGraph.Edge;
+using DotNetGraph.Extensions;
+using DotNetGraph.Node;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace ConsoleApp38
+{
+    public class StaticGraph
+    {
+        public static DotGraph graph = new DotGraph("MyGraph", true);
+
+    }
+}
